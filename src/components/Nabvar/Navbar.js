029262c7cad6db64b7ffe6d84076983ff23d529e
renzo01 <renzogomez577@gmail.com>
2019-11-03 00:38:37 -0500

    all

---
+++ src/components/Nabvar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Navbar.scss';
+import  logo from '../../img/logo.png'
+
+export default function Navar(){
+    return (
+        <nav className="navbar">
+            <img src={logo} alt="City tours logo"/>
+            <ul className="nav-links">
+                <li>
+                    <a href="/" className="nav-link">home</a>
+                </li>
+                <li>
+                    <a href="/" className="nav-link">about</a>
+                </li>
+                <li>
+                    <a href="/" className="nav-link">tours</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
